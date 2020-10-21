a05698b2e238e3091f5c7bf3f8f40b109ccea33d
devhetu <developer.designer18@gmail.com>
2020-10-21 18:06:45 +0530

    First Commit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,26 @@
 <template>
-  <div id="app" style="margin-top:0;">
-     <b-navbar toggleable="lg" type="dark" variant="dark">
+  <b-navbar toggleable="lg" type="dark" variant="info">
     <b-navbar-brand href="#">NavBar</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="/showapi" class="active">Show data</b-nav-item>
-        <b-nav-item href="/add" >Insert Data</b-nav-item>
-        <b-nav-item href="/about" >About Us</b-nav-item>
-        <b-nav-item href="/map">Google Map</b-nav-item>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          <b-form-input
+            size="sm"
+            class="mr-sm-2"
+            placeholder="Search"
+          ></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit"
+            >Search</b-button
+          >
         </b-nav-form>
 
         <b-nav-item-dropdown text="Lang" right>
@@ -37,29 +40,10 @@
         </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
-  </b-navbar><br>    
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </b-navbar>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Navbar",
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
